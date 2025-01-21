64efda713bfa9411784db2840c94bf17a98a6d47
Your Name <krushnaborase40@gmail.com>
2025-01-21 15:11:47 +0530

    kriss

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,9 +24,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <h2>Nav Item</h2>
         {children}
       </body>
     </html>
